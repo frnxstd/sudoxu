32ee1c9936d1c9b04eb20fb20779701b8cf2f653
Fornax <burakalbayrak@live.com>
2018-01-25 17:41:39 +0300

    simple usage

--- tests/test.php
+++ tests/test.php
@@ -9,14 +9,12 @@ $Sudoku = new Sudoku();
 
 $array = $Sudoku->generate('array');
 
-/*
- *
 $string = "<table cellpadding='0' cellspacing='0' style='margin:0 auto;font:30px Arial;border:3px solid black'>";
 
-for ($u = 0; $u < 16; $u++)
+for ($u = 0; $u < 9; $u++)
 {
 
-    if (($u + 1) % 4 == '0')
+    if (($u + 1) % 3 == '0')
     {
         $border = 3;
     } else {
@@ -25,10 +23,10 @@ for ($u = 0; $u < 16; $u++)
 
     $string .= '<tr>';
 
-    for ($v = 0; $v < 16; $v++)
+    for ($v = 0; $v < 9; $v++)
     {
 
-        if (($v + 1) % 4 == '0')
+        if (($v + 1) % 3 == '0')
         {
             $border2 = 3;
         } else {
@@ -39,7 +37,7 @@ for ($u = 0; $u < 16; $u++)
                         data-row="'.$u.'"
                         data-col="'.$v.'"
                         style="border: 1px solid black;border-bottom:' . $border . 'px solid black;border-right:' . $border2 . 'px solid black;line-height: 50px; width: 50px; text-align: center; vertical-align: middle;">';
-        $string .= ($array['sudoku'][$v * 16 + $u]);
+        $string .= ($array['sudoku'][$v * 9 + $u]);
         $string .= '</td>';
 
     }
@@ -52,4 +50,3 @@ $string .= "</table>";
 
 
 echo $string;
- */
